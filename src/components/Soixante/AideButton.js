6bd9b3baeb4a06ac739e10e22c5cd5bbb2d37e90
MiCetF <webmaster@micetf.fr>
2022-03-12 19:22:32 +0100

    Ajout d'une consigne.

--- src/components/Soixante/AideButton.js
+++ src/components/Soixante/AideButton.js
@@ -7,14 +7,20 @@ function AideButton({ ouvrirAide }) {
     }
 
     return (
-        <div className="d-flex justify-content-end">
-            <button
-                className="btn btn-primary"
-                onClick={handleClick}
-                title="Comment utiliser cette application web ?"
-            >
-                <Svg src={AIDE} />
-            </button>
+        <div className="d-flex justify-content-between">
+            <div className="alert alert-info">
+                Clique sur le multiple de 5 qui permet de compléter l'égalité
+                affichée dans le cadre noir.
+            </div>
+            <div className="my-auto">
+                <button
+                    className="btn btn-primary"
+                    onClick={handleClick}
+                    title="Comment utiliser cette application web ?"
+                >
+                    <Svg src={AIDE} />
+                </button>
+            </div>
         </div>
     );
 }
